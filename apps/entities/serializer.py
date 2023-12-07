24f8beeed2fa795d57d5502e7105ff8e1ebfdfab
NicolauAlmeida <114266202+NicolauAlmeida@users.noreply.github.com>
2023-12-07 18:53:56 -0300

    Delete apps/entities/serializer.py

--- apps/entities/serializer.py
+++
@@ -1,9 +0,0 @@
-from .models import Supervisor
-from rest_framework import serializers
-
-class SupervisorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Supervisor
-        fields = '__all__'
-        
-    
