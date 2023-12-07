b30c9a0571bfd3999ee7ade7c92109d7e1520eb1
NicolauAlmeida <114266202+NicolauAlmeida@users.noreply.github.com>
2023-12-07 18:54:15 -0300

    Delete apps/entities/apps.py

--- apps/entities/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SupervisorsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'supervisors'
-    verbose_name = 'Supervisores'
